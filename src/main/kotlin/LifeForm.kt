5f4a66e80f30aac5b797a768c3890bca8490b636
Paula <pauladenouden@gmail.com>
2018-06-17 13:01:02 +0200

    Add test/functions eat, reproduce, move, doNothing

--- src/main/kotlin/LifeForm.kt
+++ src/main/kotlin/LifeForm.kt
@@ -4,23 +4,42 @@ interface LifeForm {
     var age : Int
     var reproductionStage : Int
     val name : String
+    var coordinateX : Int
+    var coordinateY : Int
+
+
     //we should implement size/growth early as it affects all things and breaks user plants if implemented later - robert
     // var size : Int
     // val maxSize : Int // maxSize could influence when a lifeform can start to reproduce/starting size/cost to reproduce
     fun update(){}
     //Plant/Carnivore/Herbivore class shouldn't implement update, this is up to the user/player
 
-    fun move(){}
+    fun move(moveX: Int, moveY: Int) : Boolean {
+        if (this is Plant) {
+            return false
+        }
+        energy -= 3 // we need a way to calculate energy cost of actions
+        coordinateX += moveX
+        coordinateY += moveY
+        return true
+        // I know, I was bad, I didn't write the test first.
+    }
 
-    fun eat(){}
+    fun eat() : Boolean { // only for plants currently. needs a check if it's not a plant, it needs a lifeform to eat
+        energy += 5
+        return true
+    }
 
     fun scan(){}
 
-    fun reproduce(){}
+    fun reproduce() : LifeForm
 
     // fun grow(energy : Int){} //growth speed could depend on energy investment
 
-    fun doNothing(){}  //wait is reserved, should carry minimal energy cost(think alligators waiting for days for prey/spiders laying traps)
+    fun doNothing() : Boolean {
+        energy -= 1
+        return true
+    }  //wait is reserved, should carry minimal energy cost(think alligators waiting for days for prey/spiders laying traps)
 }
 
 class Plant : LifeForm {
@@ -29,7 +48,19 @@ class Plant : LifeForm {
     override var age = 0
     override var reproductionStage = 0
     override val name = "name"
-    //plants only eat and reproduce, they eat 'nothing' to gain energy
-    //Plant/Carnivore/Herbivore class shouldn't implement update, this is up to the user/player
+    override var coordinateX = 0
+    override var coordinateY = 0
+
     //
+    //Plant/Carnivore/Herbivore class shouldn't implement update, this is up to the user/player
+
+
+    override fun reproduce() : Plant {
+        energy -= 7
+        val childPlant = Plant()
+
+        return childPlant
+    }
+
+    //plants only eat and reproduce, they eat 'nothing' to gain energy
 }
--- src/test/kotlin/LifeFormTest.kt
+++ src/test/kotlin/LifeFormTest.kt
@@ -19,5 +19,43 @@ class TestLifeForm {
         //assert
         Assertions.assertThat(plant.energy).isNotEqualTo(10)
     }
-}
+    @Test
+    fun plantCannotMove() {
+        //assign
+        val plant = Plant()
 
+        //act
+        val moved = plant.move(1, 0)
+        //assert
+        Assertions.assertThat(moved).isEqualTo(false)
+        Assertions.assertThat(plant.coordinateX).isEqualTo(0)
+        Assertions.assertThat(plant.coordinateY).isEqualTo(0)
+        Assertions.assertThat(plant.energy).isEqualTo(10)
+    }
+    @Test
+    fun plantCanDoNothing(){
+        //assign
+        val plant = Plant()
+
+        //act
+        plant.doNothing()
+        //assert
+        Assertions.assertThat(plant.coordinateX).isEqualTo(0)
+        Assertions.assertThat(plant.coordinateY).isEqualTo(0)
+        Assertions.assertThat(plant.energy).isEqualTo(9)
+    }
+
+    @Test
+    fun plantCanReproduce(){
+        //assign
+        val plant = Plant()
+
+        //act
+        val plantChild = plant.reproduce()
+        //assert
+
+        Assertions.assertThat(plantChild).isInstanceOf(Plant::class.java)
+
+    }
+
+}
